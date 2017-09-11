1ff57c871f341328d1e4f7b286e23ac7fb7fb73e
Haolun <chshawkn@users.noreply.github.com>
2017-09-11 20:31:36 +0800

    fix log level

--- src/main/java/cn/home1/oss/environment/eureka/EurekaServer.java
+++ src/main/java/cn/home1/oss/environment/eureka/EurekaServer.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonAutoConfiguration;
 /**
  * Created by zhanghaolun on 16/9/26.
  */
-@SpringBootApplication(exclude = { RibbonAutoConfiguration.class, FeignRibbonClientAutoConfiguration.class })
+@SpringBootApplication(exclude = {RibbonAutoConfiguration.class, FeignRibbonClientAutoConfiguration.class})
 @EnableEurekaServer
 @EnableDiscoveryClient
 public class EurekaServer {
